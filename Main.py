316543eadeb6937f8f0e6c9736a4f15d7a16c183
Анастасия <stud0000222050@study.utmn.ru>
2024-03-17 02:31:00 +0500

    Add file

---
+++ Main.py
@@ -0,0 +1,21 @@
+n, m = map(int, input().split())
+a = []
+
+for i in range(n):
+    a.append(list(map(int, input().split())))
+
+sumN = list(map(sum, a))
+sumM = list(map(sum, zip(*a)))
+print(sumN)
+print(sumM)
+
+attack = 0
+cor = [0, 0]
+
+for i in range(len(sumN)):
+    for j in range(len(sumM)):
+        if sumN[i] + sumM[j] - a[i][j] > attack:
+            attack = sumN[i] + sumM[j] - a[i][j]
+            cor = [i+1, j+1]
+
+print(' '.join(map(str, cor)))
