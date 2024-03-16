db67b6d124c9cfb7b91d1d05c52907032e9cf525
Анастасия <stud0000222050@study.utmn.ru>
2024-03-16 23:35:04 +0500

    Add file

--- DZ2/E. Ambitious snail.py
+++ DZ2/E. Ambitious snail.py
@@ -1,36 +1,6 @@
 n = int(input())
-berries = {}
-max_hi = 0
-position = []
-res = []
-
-
-for i in range(1, n + 1):
-    up, down = map(int, input().split())
-    berries[i] = [up, down, up - down]
-
-print(berries)
-berries = dict(sorted(berries.items(), key=lambda item: item[1][2], reverse=True))
-berries = sorted(berries.items(), key=lambda item: item[1][0], reverse=True)
-# berries1 = dict(berries1)
-print(berries)
-pos = 0
-for el in berries:
-    res.append(el[0])
-    print(pos, end=' - ')
-    print(pos + el[1][0], end=' - ')
-    position.append(pos + el[1][0])
-    pos = pos + el[1][2]
-    print(pos)
-
-print(max(position))
-print(" ".join(map(str, res)))
-
-
-n = int(input())
 good = {}
 bed = {}
-max_hi = 0
 position = []
 res = []
 
@@ -41,21 +11,15 @@ for i in range(1, n + 1):
         good[i] = [up, down, up - down]
     else:
         bed[i] = [up, down, up - down]
-    # berries[i] = [up, down, up - down]
 
-good = sorted(good.items(), key=lambda item: item[1][2], reverse=True)
+good = sorted(good.items(), key=lambda item: item[1][1])
 bed = sorted(bed.items(), key=lambda item: item[1][0], reverse=True)
 berries = good + bed
-# berries1 = dict(berries1)
-print(berries)
 pos = 0
 for el in berries:
     res.append(el[0])
-    print(pos, end=' - ')
-    print(pos + el[1][0], end=' - ')
     position.append(pos + el[1][0])
     pos = pos + el[1][2]
-    print(pos)
 
 print(max(position))
 print(" ".join(map(str, res)))
