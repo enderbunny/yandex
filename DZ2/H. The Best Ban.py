43914bcdcdcedf66f8a141f8d7b1bd813450bdd3
Анастасия <stud0000222050@study.utmn.ru>
2024-03-18 15:25:41 +0500

    Add file

--- DZ2/H. The Best Ban.py
+++ DZ2/H. The Best Ban.py
@@ -1,21 +1,21 @@
 n, m = map(int, input().split())
 a = []
-attack = 0
 cor = [0, 0]
 
 for i in range(n):
     a.append(list(map(int, input().split())))
 
-print(a)
 sumN = list(map(sum, a))
-print(sumN)
-
-print(list(zip(*a)))
 sumM = list(map(sum, zip(*a)))
+print(sumN)
 print(sumM)
 
-cor[0] = sumN.index(max(sumN)) + 1
-cor[1] = sumM.index(max(sumM)) + 1
+attack = 0
 
+for i in range(len(sumN)):
+    for j in range(len(sumM)):
+        if sumN[i] + sumM[j] - a[i][j] > attack:
+            attack = sumN[i] + sumM[j] - a[i][j]
+            cor = [i+1, j+1]
 
 print(' '.join(map(str, cor)))
